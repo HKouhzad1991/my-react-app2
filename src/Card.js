83d58ec2980dd03db08666a5ad1629f4eec5e10a
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-13 14:28:38 +0330

    exercice chjanging function component to class component

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,27 @@
 import Button from "./Button";
 import "./Card.css";
-function Card() {
-  return (
+import React from "react";
+// function Card() {
+//   return (
+//     <div className="col">
+//       <div className="card">
+//         <img src="./images/01.png" alt="" />
+//         <div className="card-body">
+//           <h5 className="card-title">shoe1</h5>
+//           <p className="card-text">
+//             Some quick example text to build on the card title and make up the
+//             bulk of the card's content.
+//           </p>
+//           <Button />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+class Card extends React.Component{
+    render(){
+        return (
     <div className="col">
       <div className="card">
         <img src="./images/01.png" alt="" />
@@ -16,6 +36,7 @@ function Card() {
       </div>
     </div>
   );
+    }
 }
 
 export default Card;
