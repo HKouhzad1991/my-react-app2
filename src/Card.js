7c43b7ac52c2f1366af84ca58bcf96849847b89a
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-13 14:13:17 +0330

    learning react until components

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+import "./Card.css";
+function Card() {
+  return (
+    <div className="col">
+      <div className="card">
+        <img src="./images/01.png" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">shoe1</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <Button />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import Card from "./Card";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <header>header</header>
+    <section>
+      <div className="row">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </section>
+    <footer>footer</footer>
+  </>
+);
+
+
