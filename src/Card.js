58b9aa676469f4f04c6724e47ae97ffa1a6b7552
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-14 15:36:45 +0330

    default props in shoe app

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,27 @@
 import Button from "./Button";
 import "./Card.css";
 import React from "react"   ;
-// function Card() {
-//   return (
+function Card() {
+  return (
+    <div className="col">
+      <div className="card">
+        <img className='card-img' src="./images/01.png" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">shoe1</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <Button />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// class Card extends React.Component{
+//     render(){
+//         return (
 //     <div className="col">
 //       <div className="card">
 //         <img src="./images/01.png" alt="" />
@@ -17,26 +36,7 @@ import React from "react"   ;
 //       </div>
 //     </div>
 //   );
+//     }
 // }
-
-class Card extends React.Component{
-    render(){
-        return (
-    <div className="col">
-      <div className="card">
-        <img src="./images/01.png" alt="" />
-        <div className="card-body">
-          <h5 className="card-title">shoe1</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <Button />
-        </div>
-      </div>
-    </div>
-  );
-    }
-}
 
 export default Card;
