fac62fb92403da71282b23009282d1b9976f44d2
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-14 15:11:40 +0330

    learning props children

--- src/User.js
+++ src/User.js
@@ -1,12 +1,24 @@
 import "./User.css";
-function User({ name, age, height }) {
-  return (
-    <div className="container">
-      <p>name: {name}</p>
-      <p>age: {age}</p>
-      <p>height: {height}</p>
-    </div>
-  );
-}
+import React from "react";
+// function User({name , children}) {
 
+  // return (
+  //   <div className="container">
+  //     <p>name: {name}</p>
+  //     {children}
+  //   </div>
+  // );
+// }
+class User extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <p>name: {this.props.name}</p>
+        <p>age: { }</p>
+        <p>height: { }</p>
+       
+      </div>
+    );
+  }
+}
 export default User;
