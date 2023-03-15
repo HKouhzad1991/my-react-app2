88da64ecfc1a6453fb594da8625dae3d6f7cae0d
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-15 16:46:22 +0330

    service component 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import About from "./components/about/About";
 import Header from "./components/header/Header";
+import Service from "./components/service/service";
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Service />
     </>
   );
 }
--- src/components/service/service.js
+++ src/components/service/service.js
@@ -1,1 +1,28 @@
-function Service
+import './service.css'
+import ServiceItem from "./ServiceItem";
+
+function Service() {
+  return (
+    <section id="services">
+      <div className="container">
+        <div className="section-info">
+          <h2 className="section-title">SERVICES</h2>
+          <p className="section-desc">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="row">
+          <ServiceItem />
+          <ServiceItem />
+          <ServiceItem />
+          <ServiceItem />
+          <ServiceItem />
+          <ServiceItem />
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Service;
+
+
