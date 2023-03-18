2d69db91ea4137988004ce958be72634828c1073
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-18 17:51:33 +0330

    blog component creating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import About from "./components/about/About";
 import Header from "./components/header/Header";
-import Service from "./components/service/service";
+import Service from "./components/service/Service";
 
 function App() {
   return (
--- src/components/service/service.js
+++ src/components/service/service.js
@@ -1,7 +1,44 @@
-import './service.css'
+import "./Service.css";
 import ServiceItem from "./ServiceItem";
-
 function Service() {
+  let services = [
+    {
+      serviceIcon: "bi bi-briefcase",
+      serviceTitle: "WEB DESIGN",
+      serviceInfo:
+        "(1)Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+    },
+    {
+      serviceIcon: "bi bi-card-checklist",
+      serviceTitle: "WEB DEVELOPMENT",
+      serviceInfo: 
+        "(2)Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+    },
+    {
+      serviceIcon: "bi bi-bar-chart",
+      serviceTitle: "PHOTOGRAPHY",
+      serviceInfo:
+        "(3)Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+    },
+    {
+      serviceIcon: "bi bi-bar-chart",
+      serviceTitle: "PHOTOGRAPHY",
+      serviceInfo:
+        "(4)Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+    },
+    {
+      serviceIcon: "bi bi-bar-chart",
+      serviceTitle: "PHOTOGRAPHY",
+      serviceInfo:
+        "(5)Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+    },
+    {
+      serviceIcon: "bi bi-bar-chart",
+      serviceTitle: "PHOTOGRAPHY",
+      serviceInfo:
+        "(6)Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+    },
+  ];
   return (
     <section id="services">
       <div className="container">
@@ -12,17 +49,15 @@ function Service() {
           </p>
         </div>
         <div className="row">
-          <ServiceItem />
-          <ServiceItem />
-          <ServiceItem />
-          <ServiceItem />
-          <ServiceItem />
-          <ServiceItem />
+          <ServiceItem {...services[0]} />
+          <ServiceItem {...services[1]} />
+          <ServiceItem {...services[2]} />
+          <ServiceItem {...services[3]} />
+          <ServiceItem {...services[4]} />
+          <ServiceItem {...services[5]} />
         </div>
       </div>
     </section>
   );
 }
 export default Service;
-
-
