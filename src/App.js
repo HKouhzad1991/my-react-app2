b3e4e3b5489281c88bcaa388c89f32800cd82011
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-30 23:37:02 +0430

    my last push in my page app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import Service from "./components/service/Service";
 import Blog from "./components/blog/Blog";
 import Footer from "./components/footer/Footer";
 
-function App() {
-  // function handlerClick() {
-  //   console.log('کلیک شد')
-  // }
 
-  const cl
-  return (
-    <>
-      <Header />
-      <About />
-      <Service />
-      <Blog />
-      <Footer />
-    </>
+ function App() {
+   return (
+     <>
+       <Header />
+       <About />
+       <Service />
+       <Blog />
+       <Footer />
+     </>
+   );
+ }
+ export default App;
 
-    // <button onClick={handlerClick}>click me</button>
-  );
-}
-export default App;
