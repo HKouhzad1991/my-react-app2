1bebe5fa44cb2652f0f5a995f2994e44e3d4a82e
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-03-31 19:53:51 +0430

    creating container

---
+++ src/Calculator.js
@@ -0,0 +1,10 @@
+import "./Calculator.css";
+function Calculator() {
+    return (
+        <div className="container">
+            <div className="screen">
+            </div>
+        </div>;)
+}
+
+export default Calculator;
